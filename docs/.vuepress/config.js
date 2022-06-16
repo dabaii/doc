fa40e295c764c640a016ffed00ba5cf42741396b
dabai <dabai@dabaii.com>
2022-06-17 01:44:01 +0800

    添加跳转到官网得外部链接

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,20 +10,22 @@ module.exports = {
     }),
   ],
   theme: defaultTheme({
-    // navbar: [
-    //   // NavbarItem
-    //   {
-    //     text: 'Foo',
-    //     link: '/foo/',
-    //   },
-    //   // NavbarGroup
-    //   {
-    //     text: 'Group',
-    //     children: ['/group/foo.md', '/group/bar.md'],
-    //   },
-    //   // 字符串 - 页面文件路径
-    //   '/bar/README.md',
-    // ],
+    navbar: [
+      // NavbarItem
+      {
+        text: 'OASIS',
+        link: 'https://dabai-tap.vercel.app/',
+      },
+      {
+        text: 'Business Center',
+        link: 'https://dabai-tap.vercel.app/#/biz',
+      },
+      {
+        text: 'Game Center',
+        link: 'https://dabai-tap.vercel.app/#/game',
+      },
+      
+    ],
     // 侧边栏数组
     // 所有页面会使用相同的侧边栏
     sidebar: [
